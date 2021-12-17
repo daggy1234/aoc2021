be8a81cac99c82a6e05a39d63d125cf2b542702a
Arnav <60603110+Daggy1234@users.noreply.github.com>
2021-12-17 11:50:55 +0530

    run

--- day17/day17.py
+++ day17/day17.py
@@ -2,30 +2,29 @@ from typing import Tuple
 from functools import lru_cache
 import math
 
-# ta = "target area: x=20..30, y=-10..-5"
 ta = "target area: x=175..227, y=-134..-79"
+# ta = "target area: x=240..292, y=-90..-57"
+
 cds = [[int(e) for e in v[2:].split("..")] for v in ta.split(": ")[1].split(", ")]
 
 @lru_cache()
 def compute_t(iv,jv) -> Tuple[int, bool]:
 	start_loc = [0,0]
-	max_y = -99999999999999999999999
-	enoughisenough = 0
-	while enoughisenough <= 100:
+	max_y = -999999
+	while (not ((cds[0][0] <= start_loc[0] <= cds[0][1]) and (cds[1][0] <= start_loc[1] <= cds[1][1]))):
 		start_loc[0] += iv
 		start_loc[1] += jv
-		if start_loc[1] > max_y:
-			max_y = start_loc[1]
-		if (cds[0][0] <= start_loc[0] <= cds[0][1]) and (cds[1][0] <= start_loc[1] <= cds[1][1]):
-			return max_y,True
 		if iv > 0:
 			iv -= 1
 		elif iv < 0:
 			iv += 1
 		jv -= 1
-		enoughisenough += 1
+		if start_loc[1] >= max_y:
+			max_y = start_loc[1]
 
-	return 0,False
+		if (iv > cds[0][1]) or  (jv < cds[1][0]):
+			return 0,False
+	return max_y,True
 
 # def cmh(iv,jv):
 # 	v = ((iv ** 2) + (jv ** 2))
@@ -33,13 +32,17 @@ def compute_t(iv,jv) -> Tuple[int, bool]:
 # 	print((v * st) / 2)
 
 
+
+
+vvs = []
 maxs = []
 for i in range(-1000,1000):
 	for j in range(-1000,1000):
 		out,s = compute_t(i,j)
 		if s:
-			maxs.append(out)
+			maxs.append(abs(out))
 print(max(maxs))
+print(len(maxs))
 
 
 # (1225, (20, 49))
